0792282b5f1f3cfc0744632c3c26771bba931a73
Mike Cole <MikeColeGuru@gmail.com>
2018-04-30 10:06:26 -0500

    Updated Seed values

--- config/Seeds/ConfigurationsSeed.php
+++ config/Seeds/ConfigurationsSeed.php
@@ -16,14 +16,14 @@ class ConfigurationsSeed extends AbstractSeed
      *
      * @return void
      */
-    public function run()
+	public function run()
     {
         date_default_timezone_set('UTC');
         $data = [
         	[
         		'id' => 1,
         		'name' => 'Automatic Approval Time',
-        		'value' => 1,
+        		'value' => 2,
         		'created' => date('Y-m-d H:i:s'),
         		'modified' => date('Y-m-d H:i:s')
         	], [
@@ -35,7 +35,7 @@ class ConfigurationsSeed extends AbstractSeed
         	], [
         		'id' => 3,
         		'name' => 'Honoraria Booking Lead Time',
-        		'value' => 12,
+        		'value' => 10,
         		'created' => date('Y-m-d H:i:s'),
         		'modified' => date('Y-m-d H:i:s')
         	], [
@@ -47,13 +47,13 @@ class ConfigurationsSeed extends AbstractSeed
         	], [
         		'id' => 5,
         		'name' => 'Maximum Booking Lead Time',
-        		'value' => 60,
+        		'value' => 190,
         		'created' => date('Y-m-d H:i:s'),
         		'modified' => date('Y-m-d H:i:s')
         	], [
         		'id' => 6,
-        		'name' => 'Class Attendance Marked Time',
-        		'value' => 4,
+        		'name' => 'Role Call Cutoff',
+        		'value' => 2,
         		'created' => date('Y-m-d H:i:s'),
         		'modified' => date('Y-m-d H:i:s')
         	]
