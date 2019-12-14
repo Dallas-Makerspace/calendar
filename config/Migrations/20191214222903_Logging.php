508b68e197b513cdbdbc9a9e35652d12cb073b16
Andrew LeCody <andrew@beyondweb.net>
2019-12-14 16:40:25 -0600

    Added logging migration

---
+++ config/Migrations/20191214222903_Logging.php
@@ -0,0 +1,58 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Logging extends AbstractMigration
+{
+
+    public function up()
+    {
+        $this->table('logs')
+            ->addColumn('description', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('user', 'string', [
+                'default' => null,
+                'limit' => 200,
+                'null' => true,
+            ])
+            ->addColumn('ip_address', 'string', [
+                'default' => null,
+                'limit' => 100,
+                'null' => true,
+            ])
+            ->addColumn('date_time', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('url', 'string', [
+                'default' => null,
+                'limit' => 200,
+                'null' => true,
+            ])
+            ->addColumn('controller', 'string', [
+                'default' => null,
+                'limit' => 150,
+                'null' => true,
+            ])
+            ->addColumn('model', 'string', [
+                'default' => null,
+                'limit' => 150,
+                'null' => true,
+            ])
+            ->addColumn('action', 'string', [
+                'default' => null,
+                'limit' => 150,
+                'null' => true,
+            ])
+            ->create();
+    }
+
+    public function down()
+    {
+        $this->dropTable('logs');
+    }
+}
+
