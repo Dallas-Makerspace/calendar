8a789e29d3e8fbd61e35f7ef7632e02e4c3031cf
Adam Barrow <adam@thebarrows.com>
2023-03-09 12:32:46 -0600

    chore: easier local development (logs mounted locally, documentation, *nix permissions) (#156)
    * chore: map logs to a local directory
    
    * (chore) fixed volume mappings
    
    * (chore) set permissions for mount points
    
    * (chore) make local development easier
    
    * (chore) add setup.sh to quickstart
    
    * (chore) add instructions for scripts to be +x
    
    * Change to running the compose plugin (v2)
    
    * Set var/www read-write too
    
    * Cleaning up unused volumes
    
    * readability improvements

--- .docker/prod/logs.sh
+++ .docker/prod/logs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose --env-file ./.env -f ./calendar/docker-compose.prod.yml -f ./volume-override.yml logs --follow
+docker compose --env-file ./.env -f ./calendar/docker-compose.prod.yml -f ./volume-override.yml logs --follow
